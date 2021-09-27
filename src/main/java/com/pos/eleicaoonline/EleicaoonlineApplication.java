12b7b74a36330f45957b177954c2836894991db8
unknown <pedro.aleixo@gmail.com>
2021-09-26 22:48:39 -0300

    Estrutura inicial do backend

---
+++ src/main/java/com/pos/eleicaoonline/EleicaoonlineApplication.java
@@ -0,0 +1,13 @@
+package com.pos.eleicaoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleicaoonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleicaoonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pos/eleicaoonline/EleicaoonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.eleicaoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleicaoonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
