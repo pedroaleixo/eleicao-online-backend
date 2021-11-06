e18e53929bbdf9bfc714e35e04d9ba05c32b32e5
unknown <pedro.aleixo@gmail.com>
2021-11-05 22:00:55 -0300

    Redireção do frontend

--- src/main/java/br/com/eleicaoonline/controller/AutenticacaoController.java
+++ src/main/java/br/com/eleicaoonline/controller/AutenticacaoController.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.eleicaoonline.constants.Perfis;
 import br.com.eleicaoonline.domain.Administrador;
@@ -47,16 +49,18 @@ public class AutenticacaoController {
 	@Autowired
 	private EleitorService eleitorService;	
 	
+	@Value("${login.redirection.url}")
+	private String loginRedirectionUrl;		
 
-	@Operation(summary = "Realiza autenticacao do usuário")
+	@Operation(summary = "Realiza autenticacao do usuário no módulo admin")
 	@ApiResponses(value = { 
 	        @ApiResponse(responseCode = "200", description = "Sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),	       
 	        @ApiResponse(responseCode = "400", description = "Entrada inválida", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),	        
 	        @ApiResponse(responseCode = "401", description = "Usuário não autorizado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),	        
 	        @ApiResponse(responseCode = "409", description = "Erro de negócio", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),	        
 	        @ApiResponse(responseCode = "500", description = "Erro de sistema", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))) })
-	@GetMapping
-	public String gerarToken(OAuth2AuthenticationToken authentication) {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView gerarTokenAdmin(OAuth2AuthenticationToken authentication) {
 		String email = authentication.getPrincipal().getAttribute("email").toString();
 		String nome = authentication.getPrincipal().getAttribute("name").toString();
 		
@@ -80,8 +84,9 @@ public class AutenticacaoController {
 			authorithies.add(new SimpleGrantedAuthority(Perfis.ELEITOR));
 		}		
 		
-		return jwtTokenUtil.generateToken(new User(nome, "", authorithies), pessoa.getId());
-
-	}
+		String token = jwtTokenUtil.generateToken(new User(nome, "", authorithies), pessoa.getId());
+		
+		return new ModelAndView("redirect:" + loginRedirectionUrl+"?token="+token);
+	}	
 
 }
